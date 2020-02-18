6ce44d70c7b57f5ff2765833ca4066c74cc1972d
sirwan <sir1afifi@gmail.com>
2020-02-18 10:11:30 +0330

    Add custom validator

--- nosqls/mongodb/users/test/validation_test.js
+++ nosqls/mongodb/users/test/validation_test.js
@@ -2,12 +2,18 @@ const helper = require("../utils/test_helper");
 const User = require("../src/users");
 
 describe("Validating records", () => {
-  it("requires a user name", done => {
+  it("requires a user name", () => {
     const user = new User({ name: undefined });
     const validationResult = user.validateSync();
     const { message } = validationResult.errors.name;
 
     expect(message).toEqual("Name is required.");
-    done();
+  });
+
+  it("requires a user's name longer 2 charcters", () => {
+    const user = new User({ name: "Al" });
+    const validationResult = user.validateSync();
+    const { message } = validationResult.errors.name;
+    expect(message).toEqual("Name must be longer 2 characters.");
   });
 });
