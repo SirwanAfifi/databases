fc935cc04b9a1980da5055dff600b2615c8abcb1
sirwan <sir1afifi@gmail.com>
2020-02-27 10:14:27 +0330

    Add middleware to remove children

--- nosqls/mongodb/users/src/users.js
+++ nosqls/mongodb/users/src/users.js
@@ -25,6 +25,11 @@ UserSchema.virtual("postCount").get(function() {
   return this.posts.length;
 });
 
+UserSchema.pre("remove", function(next) {
+  const BlogPost = mongoose.model("blogPost");
+  BlogPost.remove({ _id: { $in: this.blogPosts } }).then(() => next());
+});
+
 const User = mongoose.model("user", UserSchema);
 
 module.exports = User;
