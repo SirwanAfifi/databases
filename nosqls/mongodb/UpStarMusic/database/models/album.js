a5eba78e5c8de6f976362c725906b5a7fdaf3e0b
sirwan <sir1afifi@gmail.com>
2020-02-27 11:28:29 +0330

    Add models

--- nosqls/mongodb/UpStarMusic/database/models/album.js
+++ nosqls/mongodb/UpStarMusic/database/models/album.js
@@ -1,1 +1,13 @@
-// Todo: create Album Schema
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const AlbumSchema = new Schema({
+  title: String,
+  date: Date,
+  copiesSold: Number,
+  numberOfTracks: Number,
+  image: String,
+  revenue: Number
+});
+
+module.exports = Album;
