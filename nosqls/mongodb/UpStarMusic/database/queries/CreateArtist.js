a75acaac91e9ec82047ec18dbd629c0d21b1e992
sirwan <sir1afifi@gmail.com>
2020-02-28 13:03:34 +0330

    Add queries

--- nosqls/mongodb/UpStarMusic/database/queries/CreateArtist.js
+++ nosqls/mongodb/UpStarMusic/database/queries/CreateArtist.js
@@ -1,9 +1,11 @@
-const Artist = require('../models/artist');
+const Artist = require("../models/artist");
 
 /**
  * Finds a single artist in the artist collection.
  * @param {object} artistProps - Object containing a name, age, yearsActive, and genre
  * @return {promise} A promise that resolves with the Artist that was created
  */
-module.exports = (artistProps) => {
+module.exports = artistProps => {
+  const artist = new Artist(artistProps);
+  return artist.save();
 };
