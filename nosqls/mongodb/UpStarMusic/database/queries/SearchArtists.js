c13b2be65780114ca60a29d77e7a758f10805b65
sirwan <sir1afifi@gmail.com>
2020-02-29 14:56:23 +0330

    Add buildQuery

--- nosqls/mongodb/UpStarMusic/database/queries/SearchArtists.js
+++ nosqls/mongodb/UpStarMusic/database/queries/SearchArtists.js
@@ -9,15 +9,7 @@ const Artist = require("../models/artist");
  * @return {promise} A promise that resolves with the artists, count, offset, and limit
  */
 module.exports = (criteria, sortProperty, offset = 0, limit = 20) => {
-  const { age, yearsActive, name } = criteria;
-  let nameCriteria = name ? { name } : {};
-  let ageCriteria = age ? { age: { $lte: age.max, $gte: age.min } } : {};
-  let yearsActiveCriteria = yearsActive
-    ? { yearsActive: { $lte: yearsActive.max, $gte: yearsActive.min } }
-    : {};
-
-  const search = { ...nameCriteria, ...ageCriteria, ...yearsActiveCriteria };
-  const query = Artist.find(search)
+  const query = Artist.find(buildQuery(criteria))
     .sort({ [sortProperty]: 1 })
     .skip(offset)
     .limit(limit);
@@ -30,4 +22,30 @@ module.exports = (criteria, sortProperty, offset = 0, limit = 20) => {
       limit
     };
   });
+};
+
+const buildQuery = criteria => {
+  const query = {};
+
+  if (criteria.name) {
+    query.$text = {
+      $search: criteria.name
+    };
+  }
+
+  if (criteria.age) {
+    query.age = {
+      $gte: criteria.age.min,
+      $lte: criteria.age.max
+    };
+  }
+
+  if (criteria.yearsActive) {
+    query.yearsActive = {
+      $gte: criteria.yearsActive.min,
+      $lte: criteria.yearsActive.max
+    };
+  }
+
+  return query;
 };
