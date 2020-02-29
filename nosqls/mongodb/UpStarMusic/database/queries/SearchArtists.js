05f94e4685c21a099c95aed775ac6d076955580f
sirwan <sir1afifi@gmail.com>
2020-02-29 12:19:13 +0330

    Add a complex query

--- nosqls/mongodb/UpStarMusic/database/queries/SearchArtists.js
+++ nosqls/mongodb/UpStarMusic/database/queries/SearchArtists.js
@@ -1,4 +1,4 @@
-const Artist = require('../models/artist');
+const Artist = require("../models/artist");
 
 /**
  * Searches through the Artist collection
@@ -9,4 +9,25 @@ const Artist = require('../models/artist');
  * @return {promise} A promise that resolves with the artists, count, offset, and limit
  */
 module.exports = (criteria, sortProperty, offset = 0, limit = 20) => {
+  const { age, yearsActive, name } = criteria;
+  let nameCriteria = name ? { name } : {};
+  let ageCriteria = age ? { age: { $lte: age.max, $gte: age.min } } : {};
+  let yearsActiveCriteria = yearsActive
+    ? { yearsActive: { $lte: yearsActive.max, $gte: yearsActive.min } }
+    : {};
+
+  const search = { ...nameCriteria, ...ageCriteria, ...yearsActiveCriteria };
+  const query = Artist.find(search)
+    .sort({ [sortProperty]: 1 })
+    .skip(offset)
+    .limit(limit);
+
+  return Promise.all([query, Artist.count()]).then(results => {
+    return {
+      all: results[0],
+      count: results[1],
+      offset,
+      limit
+    };
+  });
 };
