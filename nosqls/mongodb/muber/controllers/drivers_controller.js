badfb6f84ccc7aea24f537bb692d5122d28ebd75
sirwan <sir1afifi@gmail.com>
2020-03-04 09:59:10 +0330

    Add edit handler

--- nosqls/mongodb/muber/controllers/drivers_controller.js
+++ nosqls/mongodb/muber/controllers/drivers_controller.js
@@ -10,5 +10,14 @@ module.exports = {
     Driver.create(driverProps)
       .then(driver => res.send(driver))
       .catch(next);
+  },
+  edit(req, res, next) {
+    const driverId = req.params.id;
+    const driverProps = req.body;
+
+    Driver.findByIdAndUpdate(driverId, driverProps)
+      .then(() => Driver.findById({ _id: driverId }))
+      .then(driver => res.send(driver))
+      .catch(next);
   }
 };
--- nosqls/mongodb/muber/routes/routes.js
+++ nosqls/mongodb/muber/routes/routes.js
@@ -4,4 +4,5 @@ module.exports = app => {
   app.get("/api", DriverController.gretting);
 
   app.post("/api/drivers", DriverController.create);
+  app.put("/api/drivers/:id", DriverController.edit);
 };
